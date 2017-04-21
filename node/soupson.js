9332bae54843f313080cbbdfc2660312e5901f9c
Bilel Memdouh <bilel.memdouh@savoirfairelinux.com>
2017-04-21 16:31:22 -0400

    first change :-|

--- node/soupson.js
+++ node/soupson.js
@@ -2,16 +2,17 @@ var request = require('request');
 var cheerio = require('cheerio');
 var AsciiTable = require('ascii-table');
 var moment = require('moment');
-var table = new AsciiTable();
+var table = '';
+var newLine = '\n';
 
 request("http://www.soupson.ca/?lang=en", function(error, response, body){
   $ = cheerio.load(body);
-  table.setTitle($('.entry-title').first().text());
+  //table.setTitle($('.entry-title').first().text());
 
   var dayMenu = $('.entry-content').first().find('p').eq(moment().day()).text();
   var weekMenu = $('.entry-content').slice(0,5).find('p').slice(1,6);
   var result = "";
   for (var i = 0, len = weekMenu.length; i < len; i++)
-    table.addRow((weekMenu[i].children[0].data).split(': '));
-  console.log(table.toString());
+    table += weekMenu[i].children[0].data + newLine;
+  console.log(table);
 });
